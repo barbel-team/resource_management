c781c4ad25e86e76c828d09a111c890c5b112d72
Identityless <rlandwnd555@naver.com>
2022-12-21 20:59:43 +0900

    init

---
+++ src/test/java/com/barbel/resource_management/ResourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbel.resource_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
